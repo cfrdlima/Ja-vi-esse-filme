e9b53e4124bc4e1027ec54c3d5cb48837bf8c42c
Claudinei de Lima <cfrdlxava@gmail.com>
2024-09-03 18:21:16 -0300

    add: manifest pwa

--- movies-app/src/app/layout.tsx
+++ movies-app/src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link ref="manifest" href="%PUBLIC_URL%/manifest.json"></link>
       <body className={inter.className}>{children}</body>
     </html>
   );
