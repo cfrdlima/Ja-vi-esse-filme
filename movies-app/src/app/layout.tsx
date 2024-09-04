bbfab2a7e3b2258344b6dff6a4918fd87e47784f
Claudinei de Lima <cfrdlxava@gmail.com>
2024-09-04 20:53:47 -0300

    add: speed insights

--- movies-app/src/app/layout.tsx
+++ movies-app/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <link rel="manifest" href="/manifest.json"></link>
+      <SpeedInsights />
       <body className={inter.className}>{children}</body>
     </html>
   );
