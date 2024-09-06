b1aa8a960ed81a9e7e7704d97a7c849f1d69fef3
Claudinei de Lima <cfrdlxava@gmail.com>
2024-09-06 17:07:13 -0300

    add: metatags fix: npms e metatags

--- movies-app/src/app/layout.tsx
+++ movies-app/src/app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Head from "next/head"; // Importando o Head do Next.js
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Já vi esse filme?",
-  description: "Gerencie seus filmes e series",
+  description: "Gerencie seus filmes e séries",
 };
 
 export default function RootLayout({
@@ -17,9 +18,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="manifest" href="/manifest.json"></link>
-      <SpeedInsights />
-      <body className={inter.className}>{children}</body>
+      <Head>
+        <title>Já vi esse filme?</title>
+        <meta name="description" content="Gerencie seus filmes e séries." />
+        <meta
+          name="keywords"
+          content="filmes, séries, gerenciamento, catálogo"
+        />
+        <meta property="og:title" content="Já vi esse filme?" />
+        <meta
+          property="og:description"
+          content="Gerencie seus filmes e séries."
+        />
+        <meta property="og:image" content="https://seusite.com/imagem.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <body className={inter.className}>
+        <SpeedInsights />
+        {children}
+      </body>
     </html>
   );
 }
