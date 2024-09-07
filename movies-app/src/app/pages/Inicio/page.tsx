8666717c195b59104f572cc08286a816629cae3c
Claudinei de Lima <cfrdlxava@gmail.com>
2024-09-07 20:50:55 -0300

    add: footer

--- movies-app/src/app/pages/Inicio/page.tsx
+++ movies-app/src/app/pages/Inicio/page.tsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 import Navbar from "@/components/navbar/page";
 import { BiSearchAlt2 } from "react-icons/bi";
 import movieBackground from "../../assets/movie_background.jpg";
+import linkedinLogo from "../../assets/icons8-linkedin.svg";
+import gitHubLogo from "../../assets/github-mark-white.svg";
 import { IoIosArrowForward } from "react-icons/io";
 import ReactLoading from "react-loading";
 import { useMovies } from "@/hooks/useMovies";
@@ -140,6 +142,53 @@ export default function HomePage() {
           <AuxiliarScrollMovie movies={moviesPopular} />
         </div>
       </section>
+      <footer className="homePage-footer-container">
+        <div className="homePage-footer">
+          <div className="homePage-footerTitle">Já vi esse filme?</div>
+          <div className="homePage-footerText">
+            O Já vi esse filme? é uma plataforma de recomendação de filmes e
+            séries, onde você pode ver informações sobre os filmes e séries mais
+            populares, melhores avaliados, lançamentos e muito mais.
+          </div>
+        </div>
+        <div className="homePage-footer">
+          <div className="homePage-footerTitle">Contato</div>
+          <div className="homePage-footerText-contact">
+            <div>feedback.portifolio@gmail.com</div>
+          </div>
+        </div>
+        <ul className="homePage-footer-links">
+          <ul>
+            <a
+              href="https://www.linkedin.com/in/claudinei-de-lima-690b4021a/"
+              target="_blank"
+            >
+              <Image
+                className="homePage-footer-links-images"
+                src={linkedinLogo}
+                alt="instagram"
+                quality={100}
+              />
+            </a>
+          </ul>
+          <ul>
+            <a
+              href="https://github.com/cfrdlima/Ja-vi-este-filme--"
+              target="_blank"
+            >
+              <Image
+                className="homePage-footer-links-images-2"
+                src={gitHubLogo}
+                alt="repositorio"
+                quality={100}
+              />
+            </a>
+          </ul>
+        </ul>
+        <p className="footer__copyright">
+          © 2023 Claudinei de Lima. Todos os direitos reservados.
+        </p>
+      </footer>
     </>
   );
 }
