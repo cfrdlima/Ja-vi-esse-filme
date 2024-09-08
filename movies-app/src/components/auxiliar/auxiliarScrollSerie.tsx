a766392825d489846760aff09dd1eccb70e7a300
Claudinei de Lima <cfrdlxava@gmail.com>
2024-09-07 23:24:41 -0300

    add: slider nas series

--- movies-app/src/components/auxiliar/auxiliarScrollSerie.tsx
+++ movies-app/src/components/auxiliar/auxiliarScrollSerie.tsx
@@ -1,34 +1,51 @@
-import { useRef } from "react";
 import "./auxiliarScrollMovie.scss";
 import { Series } from "@/types/series";
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export interface Props {
   series: Series[];
 }
 
 export default function AuxiliarScrollMovie({ series }: Props) {
-  const movieListRef = useRef<HTMLUListElement>(null);
-
-  const scrollLeft = () => {
-    if (movieListRef.current) {
-      movieListRef.current.scrollTo({
-        left:
-          movieListRef.current.scrollLeft -
-          movieListRef.current.clientWidth * 0.5,
-        behavior: "smooth",
-      });
-    }
-  };
-
-  const scrollRight = () => {
-    if (movieListRef.current) {
-      movieListRef.current.scrollTo({
-        left:
-          movieListRef.current.scrollLeft +
-          movieListRef.current.clientWidth * 0.5,
-        behavior: "smooth",
-      });
-    }
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToScroll: 4,
+    slidesToShow: 11,
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 8,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   const handleSeeMore = (serie: Series) => {
@@ -39,31 +56,22 @@ export default function AuxiliarScrollMovie({ series }: Props) {
   };
 
   return (
-    <div className="movie-list-container">
-      <ul className="movie-list" ref={movieListRef}>
+    <div className="slider-container">
+      <Slider {...settings}>
         {series.map((serie) => (
-          <li
-            className="movie-card"
+          <div
             key={serie.id}
+            className="movie-slide"
             onClick={() => handleSeeMore(serie)}
           >
-            <div className="movie-poster">
-              <img
-                src={`https://image.tmdb.org/t/p/original${serie.poster_path}`}
-                alt={serie.name}
-              />
-            </div>
-          </li>
+            <img
+              src={`https://image.tmdb.org/t/p/original${serie.poster_path}`}
+              alt={serie.name}
+              className="movie-image"
+            />
+          </div>
         ))}
-      </ul>
-      <div className="movie-list-nav">
-        <button className="prev" onClick={scrollLeft}>
-          &#10094;
-        </button>
-        <button className="next" onClick={scrollRight}>
-          &#10095;
-        </button>
-      </div>
+      </Slider>
     </div>
   );
 }
